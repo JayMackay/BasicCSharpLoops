9f1e65e2bd1074e5fe073a6998dc31ef629b7f4d
JayMackay <jared.mackay1994@live.com>
2020-02-26 15:24:16 +0000

    Array Loop Application With Unit Tests

---
+++ TestLoopProgram/MethodLoopsUnitTest/LoopsUnitTest.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using System;
+using TestLoopProgram;
+
+namespace MethodLoopsUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void LowestForLoopMethodTest()
+        {
+            //ACT
+            int[] nums = { 5, 10, 15, 16, 5, 2 };
+            var result = LoopMethods.LowestForLoop(nums);
+            //ASSERT
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void ForEachTest()
+        {
+            //ACT
+            int[] nums = { 1, 2, 3, 4, 5 };
+            var result = LoopMethods.LowestForEach(nums);
+            //ASSERT
+            Assert.AreEqual(1, result);
+
+        }
+
+        [TestCase(2,0)]
+        [Test]
+        public void calcDiv(int num1, int num2)
+        {
+            //ACT
+            var exception = Assert.Throws<ArgumentException>(() =>
+            LoopMethods.calcDiv(num1, num2));
+            //ASSERT
+            Assert.AreEqual($"Can not divide by {num2}", exception);
+        }
+
+    }
+}
+
+
+
+
+//ACT
+var exception = Assert.Throws<ArgumentException>(() =>
+    LoopMethods.calcDiv(num1, num2));
+//ASSERT
+Assert.AreEqual($"Can not divide by {num2}", exception.Message);
---
+++ TestLoopProgram/TestLoopProgram/LoopMethods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TestLoopProgram
+{
+    public class LoopMethods
+    {
+        //FOR LOOP METHOD
+        public static int LowestForLoop(int[] nums)
+        {
+            int lowest = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < lowest)
+                {
+                    lowest = nums[i];
+                }
+            }
+            return lowest;
+        }
+
+        //FOR EACH METHOD
+        public static int LowestForEach(int[] nums)
+        {
+            int lowest = nums[0];
+            foreach(int i in nums)
+            {
+                if(i < lowest)
+                {
+                    lowest = i;
+                }
+            }
+            return lowest;
+        }
+
+        public static int calcDiv(int num1, int num2)
+        {
+            try
+            {
+                int calcDivOutput = num1 / num2;
+            }
+            catch(Exception)
+            {
+                throw new ArgumentException($"Can not divide by {num2}");
+            }
+            return num1 / num2;
+        }
+        
+        
+    }
+}
